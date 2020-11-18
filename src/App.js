d14d818e37c9eab81649029388d8bfcdb9b60b57
RamlahAziz <44732722+RamlahAziz@users.noreply.github.com>
2020-11-18 23:51:35 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import ToDo from "./components/ToDo";
 import "./App.css";
+import UserContainer from "./components/UserContainer";
 
 function App() {
+    const [showToDo, setShowToDo] = useState(true);
+    const handleClick = () => setShowToDo(showToDo => !showToDo);
     return (
         <div className="App">
-            <ToDo />
+            {showToDo ? <ToDo /> : <UserContainer />}
+            <button class="appDisplayButton" onClick={handleClick}>
+                {showToDo ? "Fetch Users" : "Hide Users"}
+            </button>
         </div>
     );
 }
